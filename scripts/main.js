2ae5158dde39d49e7095b0362a10aa9fa0ec4959
Trevor Shibley <trevor@tshibley.com>
2021-04-21 21:49:49 -0700

    pogpete

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,17 @@ let dragging = false;
 
 const renderSandwich = (document, x, y) => {
   var elem = document.createElement("img");
-  elem.src = "assets/sandwich.png";
-  elem.setAttribute("class", "sandwich");
+
+  // make every 100th sandwich a pog pete
+  if (sandwichCount > 0 && sandwichCount % 100 === 0) {
+    elem.src = "assets/pogpete.png";
+    elem.setAttribute("class", "pogpete");
+  } else {
+    elem.src = "assets/sandwich.png";
+    elem.setAttribute("class", "sandwich");
+  }
+
+  elem.setAttribute("class", elem.className + " sandwichLike");
   elem.setAttribute("id", `sandwich-${sandwichCount}`);
   elem.setAttribute("draggable", false);
   elem.style.top = `${y - 12}px`;
