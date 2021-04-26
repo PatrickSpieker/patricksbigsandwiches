41de34725d42411c7662d62c75034ecac60cc80c
Trevor Shibley <trevor@tshibley.com>
2021-04-25 20:58:54 -0700

    [WIP] Sandwich vs. Pog pete ratio slider  (#8)
    * save work
    
    * z index of slider
    
    * rudimentary slider, some logic to work out
    
    * working well enough for now

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,21 @@
 let sandwichCount = 0;
 let dragging = false;
 
+const lastDrawnCoors = [0, 0];
+
 const renderSandwich = (document, x, y) => {
   var elem = document.createElement("img");
 
-  // make every 100th sandwich a pog pete
-  if (sandwichCount > 0 && sandwichCount % 100 === 0) {
-    elem.src = "assets/pogpete.png";
-    elem.setAttribute("class", "pogpete");
-  } else {
+  // randomly choose pogpete vs. sandwich based on ratio
+  const first = Math.random() * 10;
+  const second = Math.sqrt(pogPeteSlider.value);
+  console.log(first, second);
+  if (first > second) {
     elem.src = "assets/sandwich.png";
     elem.setAttribute("class", "sandwich");
+  } else {
+    elem.src = "assets/pogpete.png";
+    elem.setAttribute("class", "pogpete");
   }
 
   elem.setAttribute("class", elem.className + " sandwichLike");
@@ -40,7 +45,19 @@ document.addEventListener("mouseup", () => {
 
 document.addEventListener("mousemove", function (e) {
   if (dragging) {
-    renderSandwich(document, e.clientX, e.clientY);
-    sandwichCount++;
+    const x = e.clientX;
+    const y = e.clientY;
+    if (
+      Math.abs(x - lastDrawnCoors[0]) < 5 ||
+      Math.abs(y - lastDrawnCoors[1]) < 5
+    ) {
+    } else {
+      lastDrawnCoors[0] = x;
+      lastDrawnCoors[1] = y;
+      renderSandwich(document, e.clientX, e.clientY);
+      sandwichCount++;
+    }
   }
 });
+
+const pogPeteSlider = document.getElementById("pogpeteslider");
