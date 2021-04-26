18be549421470e88bb00f0aaa151d7a946b0e927
Trevor Shibley <trevor@tshibley.com>
2021-04-25 18:31:24 -0700

    rudimentary slider, some logic to work out

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 let sandwichCount = 0;
 let dragging = false;
+let pogpeteratio = 1;
+
+const lastDrawnCoors = [0, 0];
 
 const renderSandwich = (document, x, y) => {
   var elem = document.createElement("img");
 
-  // make every 100th sandwich a pog pete
-  if (sandwichCount > 0 && sandwichCount % 100 === 0) {
+  // randomly choose pogpete vs. sandwich based on ratio
+  if (Math.random() * 10 < Math.sqrt(pogpeteratio)) {
     elem.src = "assets/pogpete.png";
     elem.setAttribute("class", "pogpete");
   } else {
@@ -40,7 +43,45 @@ document.addEventListener("mouseup", () => {
 
 document.addEventListener("mousemove", function (e) {
   if (dragging) {
-    renderSandwich(document, e.clientX, e.clientY);
-    sandwichCount++;
+    const x = e.clientX;
+    const y = e.clientY;
+    if (
+      Math.abs(x - lastDrawnCoors[0]) < 5 ||
+      Math.abs(y - lastDrawnCoors[1]) < 5
+    ) {
+      console.log("Returning", x, lastDrawnCoors[0], y, lastDrawnCoors[1]);
+    } else {
+      lastDrawnCoors[0] = x;
+      lastDrawnCoors[1] = y;
+      renderSandwich(document, e.clientX, e.clientY);
+      sandwichCount++;
+    }
   }
 });
+
+const pogPeteSlider = document.getElementById("pogpeteslider");
+
+pogPeteSlider.oninput = () => {
+  pogpeteratio = pogPeteSlider.value;
+};
+
+const throttle = (func, limit) => {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
